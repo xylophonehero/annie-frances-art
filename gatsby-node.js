6d01767ef0d07783c55b3807a29e24364bca175b
xylophonehero <nick97000@gmail.com>
2022-10-23 16:51:08 +0100

    remove netlify functions

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,4 @@
 const path = require('path')
-// const { getSlug } = require('./src/utils/GetSlug')
 const slugify = require('slugify')
 
 exports.createPages = async ({ graphql, actions }) =>
@@ -9,11 +8,6 @@ exports.createPages = async ({ graphql, actions }) =>
   const paintingTemplate = path.resolve('src/templates/paintingPage.js')
   const result = await graphql(`
     query{
-      allContentfulPainting(filter:{commission: {ne: true}}){
-        nodes{
-          name
-        }
-      }
       allContentfulPage{
         nodes{
           title
@@ -22,17 +16,6 @@ exports.createPages = async ({ graphql, actions }) =>
     }
   `)
 
-  // result.data.allContentfulPainting.nodes.map(node =>
-  // {
-  //   const slug = slugify(node.name, { lower: true })
-  //   createPage({
-  //     path: `paintings/${slug}`,
-  //     component: paintingTemplate,
-  //     context: {
-  //       name: node.name
-  //     }
-  //   })
-  // })
   result.data.allContentfulPage.nodes.map(node =>
   {
     const slug = node.title === "Home" ? "/" : slugify(node.title, { lower: true })
@@ -45,21 +28,3 @@ exports.createPages = async ({ graphql, actions }) =>
     })
   })
 }
-
-const util = require("util");
-const child_process = require("child_process");
-const exec = util.promisify(child_process.exec);
-
-exports.onPostBuild = async (gatsbyNodeHelpers) =>
-{
-  const { reporter } = gatsbyNodeHelpers;
-
-  const reportOut = (report) =>
-  {
-    const { stderr, stdout } = report;
-    if (stderr) reporter.error(stderr);
-    if (stdout) reporter.info(stdout);
-  };
-
-  reportOut(await exec("yarn lambda"));
-};
